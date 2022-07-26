4bc11ffab04af03969df03477325f15c8f26f0a5
GITGARDS <adrianorogeriodesouza4353@gmail.com>
2022-07-26 15:04:40 -0300

    AULA 14 - PÁGINA VISUALIZAR DENÚNCIA SELECIONADA

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,14 @@ router.get('/denuncia', function(req, res, next) {
   res.render('usuario/denuncia');
 });
 
+/* GET nova denuncia-selecionada. */
+router.get('/denuncia/:id', function(req, res, next) {
+  res.render('usuario/denuncia-selecionada');
+});
+
+/* GET nova usuario-upate. */
+router.get('/user/:id', function(req, res, next) {
+  res.render('usuario/user-update');
+});
+
 module.exports = router;
